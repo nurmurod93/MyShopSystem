dfc70820184d5ce7b1286a85d730d2dac4709665
Abdullajon <149893285+nurmurod93@users.noreply.github.com>
2025-03-02 17:08:59 +0500

    Login Register

--- MyShopSystem_UI/Program.cs
+++ MyShopSystem_UI/Program.cs
@@ -1,7 +1,10 @@
+using Blazored.LocalStorage;
 using MyShopSystem_UI.Components;
 using MyShopSystem_UI.Service;
+using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddBlazoredLocalStorage(); // Xizmatni qo‘shish
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -16,6 +19,8 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IWarehouseService, WarehouseService>();
 builder.Services.AddScoped<IReceiveService, ReceiveService>();
 builder.Services.AddScoped<ISendService, SendService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5109/") });
+
 
 var app = builder.Build();
 
---
+++ MyShopSystem_UI/Service/AutherticationService.cs
@@ -0,0 +1,27 @@
+﻿namespace MyShopSystem_UI.Service
+{
+    public class AutherticationService
+    {
+        private readonly HttpClient client;
+
+        public AutherticationService(HttpClient httpClient)
+        {
+            this.client = httpClient;
+        }
+        public async Task<bool> RegisterAsync(string email, string password)
+        {
+            var result = await client.PostAsJsonAsync("api/account/register", new { Email = email, Password = password });
+            return result.IsSuccessStatusCode;
+        }
+        public async Task<string?> loginAsync(string email, string password)
+        {
+            var response = await client.PostAsJsonAsync("api/account/login", new { Email = email, Password = password});
+            if(response.IsSuccessStatusCode)
+            {
+                var token = await response.Content.ReadAsStringAsync();
+                return token;
+            }
+            return null;
+        }
+    }
+}
